08d87305293d573be56c398ebc6cd0a27dfbee3b
jiandandaoxingfu <jiaminxin@outlook.com>
2020-02-10 00:55:16 +0800

    添加线程数输入框。

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@
 import React from 'react';
 import './App.css';
 import styles from './styles.js';
-import { Button, Layout, Input, Table, Progress } from 'antd';
+import { Button, Layout, Input, Table, Progress, InputNumber } from 'antd';
 
 const { Header, Content, Footer } = Layout;
 
@@ -20,6 +20,8 @@ class App extends React.Component {
             	    <Input placeholder="输入文章标题，多篇以逗号隔开" style = { styles.input_title } id='title'/>
                     <Input placeholder="筛选年份" style = { styles.input_year } id='year'/>
                     <Input placeholder="作者姓名：如 Chen-Jing-Run" style = { styles.input_author } id='author'/>
+                    <span style = { {fontSize: '15px', color: 'rgba(0, 0, 0, 0.3)'} }>最大下载数： </span>
+                    <InputNumber min={1} max={5} defaultValue={3} id="threads"/>
             	    <Button type='primary' style={ styles.button } >开始统计</Button>
             	    <Button type='primary' style={ styles.button } >重新启动</Button>
                 </Header>
@@ -27,7 +29,7 @@ class App extends React.Component {
                     <div id="container" style = { styles.container }></div>
                 </Content>
                 <Footer style = { styles.footer }>
-            		©2020 Created by JMx. <a href='javascript: electron.shell.openExternal("https://github.com/jiandandaoxingfu/sci-info-collect/blob/master/README.md")' >帮助</a><br />
+            		©2020 Created by JMx. <a href="https://github.com/jiandandaoxingfu/sci-info-collect/blob/master/README.md">帮助</a><br />
                 </Footer>
             </Layout>
         );
